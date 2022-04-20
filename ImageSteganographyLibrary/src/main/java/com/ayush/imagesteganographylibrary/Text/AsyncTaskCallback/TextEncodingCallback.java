c82c422805e9eeb138d18675800b27438c32b7b2
eliteextant <kajaldadas149@gmail.com>
2022-04-20 12:46:48 +0530

    Initial commit

---
+++ ImageSteganographyLibrary/src/main/java/com/ayush/imagesteganographylibrary/Text/AsyncTaskCallback/TextEncodingCallback.java
@@ -0,0 +1,15 @@
+package com.kajal.imagesteganographylibrary.Text.AsyncTaskCallback;
+
+import com.kajal.imagesteganographylibrary.Text.ImageSteganography;
+
+/**
+ * This the callback interface for TextEncoding AsyncTask.
+ */
+
+public interface TextEncodingCallback {
+
+    void onStartTextEncoding();
+
+    void onCompleteTextEncoding(ImageSteganography result);
+
+}
